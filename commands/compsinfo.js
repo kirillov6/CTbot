b14957ca21f1cf81d5cd211e406dba8b55cd5ed0
Yaroslav Kirillov <kirillov.y6@gmail.com>
2021-08-23 15:48:36 +0500

    add compsinfo command

---
+++ commands/compsinfo.js
@@ -0,0 +1,36 @@
+// Команда !compsinfo отвечает за получение информации о компонентах
+
+// Импорт
+const path    = require('../utils/path');
+const file    = require('../utils/file');
+const Discord = require('discord.js');
+
+
+module.exports = {
+    name: 'compsinfo',
+    description: 'Получить информацию о компонентах',
+    turnedOn: true,     // Включить/Выключить доступность команды
+    args: true,         // Есть ли аргументы
+    min_args: 1,        // Минимальное количество аргументов
+    max_args: 1,        // Максимальное количество аргументов
+
+    execute(message, args) {
+        
+        if (args[0] == "ver") {
+
+            var listOfInstructions = {
+                "1. Открываем компонент на редактирование:": file.COMPSINFO_VER_1,
+                "2. Смотрим версию компонента:": file.COMPSINFO_VER_2,
+                "2. Соотносим версию компонента с версией мастера:": file.COMPSINFO_VER_3
+            }
+
+            for (var key in listOfInstructions) {
+                if (listOfInstructions.hasOwnProperty(key)) {
+                    message.channel.send(key, new Discord.MessageAttachment(`${path.IMG}/${listOfInstructions[key]}`));
+                }
+            }
+        }
+        else
+            return utils.MsgReplyAndDelete(message, str.COMMAND_BADFORMAT_ARGS);
+    }
+};
--- utils/file.js
+++ utils/file.js
@@ -2,5 +2,8 @@
 
 module.exports = {
 
-    WI_SCHEME                  : 'WIscheme.png',
+    WI_SCHEME               : 'WIscheme.png',
+    COMPSINFO_VER_1         : 'CompsInfo/ver_1.png',
+    COMPSINFO_VER_2         : 'CompsInfo/ver_2.png',
+    COMPSINFO_VER_3         : 'CompsInfo/ver_3.png'
 }
